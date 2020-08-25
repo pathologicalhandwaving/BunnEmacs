9f1c48ec0866c001e69fd82ddb992457beca5415
PathologicalHandwaving <kristimshort@icloud.com>
2020-08-25 15:25:23 -0400

    config.el

--- config.el
+++ config.el
@@ -107,23 +107,85 @@
 
 (load-theme 'doom-nord t)
 
-(use-package neotree
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
   :config
-  (neotree-dir "~/.emacs.d"))
+  (progn
+    (setq treemacs-collapse-dirs (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay 0.5
+          treemacs-directory-name-transformer #'identity
+          treemacs-display-in-side-window t
+          treemacs-eldoc-display t
+          treemacs-file-event-delay 5000
+          treemacs-file-extension-regex treemacs-last-period-regex-value
+          treemacs-file-follow-delay 0.2
+          treemacs-file-name-transformer #'identity
+          treemacs-follow-after-init t
+          treemacs-git-command-pipe ""
+          treemacs-goto-tag-strategy 'refetch-index
+          treemacs-indentation 2
+          treemacs-indentation-string " "
+          treemacs-is-never-other-window nil
+          treemacs-max-git-entries 5000
+          treemacs-missing-project-action 'ask
+          treemacs-moce-forward-on-expand nil
+          treemacs-no-png-images nil
+          treemacs-no-delete-other-windows t
+          treemacs-project-follow-cleanup nil
+          treemacs-persist-file (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position 'left
+          treemacs-recenter-distance 0.1
+          treemacs-recenter-after-file-follow nil
+          treemacs-recenter-after-tag-follow nil
+          treemacs-show-cursor nil
+          treemacs-show-hidden-files t
+          treemacs-silent-filewatch nil
+          treemacs-silent-refresh nil
+          treemacs-sorting 'alphabetic-asc
+          treemacs-space-between-root-nodes t
+          treemacs-tag-follow-cleanup t
+          treemacs-tag-follow-delay 1.5
+          treemacs-user-mode-line-format nil
+          treemacs-user-header-line-format nil
+          treemacs-width 35
+          treemacs-workspace-switch-cleanup nil)
 
-(global-set-key [f8] 'neotree-toggle)
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode t)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+        ("M-0" . treemacs-select-window)
+        ("C-x t 1" . treemacs-delete-other-windows)
+        ("C-x t t" . treemacs)
+        ("C-x t B" . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
 
-(defun neotree-startup ()
-  (interactive)
-  (neotree-show)
-  (call-interactively 'other-window))
-(if (daemonp)
-    (add-hook 'server-switch-hook #'neotree-startup)
-  (add-hook 'after-init-hook #'neotree-startup))
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :ensure t)
 
-(use-package all-the-icons)
+(use-package treemacs-all-the-icons)
 
-(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+(use-package treemacs-icons-dired
+  :after treemacs dired
+  :ensure t
+  :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after treemacs magit
+  :ensure t)
 
 (use-package nyan-mode
   :config
@@ -747,6 +809,11 @@ For example: \"<e\" -> (\"e\" . t)"
 
 (setq org-startup-folded t)
 
+(require 'org-gcal)
+(setq org-gcal-client-id "665892281799-neavl78mkd8v47ng50dpdanii7j42rkv.apps.googleusercontent.com"
+      org-gcal-client-secret "G3-17CMyU-2x3oCDP-I40ZTO"
+      org-gcal-fetch-file-alist '(("kms.orgsync@gmail.com" ~/OrgDB/Inbox/agenda.org)))
+
 (setq org-archive-location "~/OrgDB/Archive/agenda.org_archive::datetree/* Completed Tasks")
 
 (defun bun-create-notes-file ()
@@ -848,6 +915,10 @@ For example: \"<e\" -> (\"e\" . t)"
 
 (require 'org-download)
 (setq-default org-download-image-dir "~/OrgDB/Notes")
+
+(require 'org-web-tools)
+
+(require 'org-kanban)
 
 (require 'org-books)
 (setq org-books-file '("~/Librarian/index.org"))
